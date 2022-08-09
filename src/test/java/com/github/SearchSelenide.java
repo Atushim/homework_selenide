6b5a4579f98869958457045575bd286c7b2d2562
Daniil <mishuta_daniil@mail.ru>
2022-08-09 16:38:23 +0300

    reformat code

--- src/test/java/com/github/SearchSelenide.java
+++ src/test/java/com/github/SearchSelenide.java
@@ -10,23 +10,21 @@ import static com.codeborne.selenide.Selenide.*;
 public class SearchSelenide {
 
 
-
-
     @Test
-    void testSearchSelenide(){
+    void testSearchSelenide() {
         //Открываем странциу selenide
-    open("https://github.com/");
-    $("[data-test-selector=nav-search-input]").setValue("selenide").pressEnter();
-    $("ul.repo-list li a").click();
-    // Переходим в раздел Wiki
-    $("#wiki-tab").click();
-    //Открываем полностью список страниц и проверяем наличие SoftAssertions
-    $(byText("Show 2 more pages…")).click();
-    $("#wiki-pages-box").shouldHave(textCaseSensitive("SoftAssertions"));
+        open("https://github.com/");
+        $("[data-test-selector=nav-search-input]").setValue("selenide").pressEnter();
+        $("ul.repo-list li a").click();
+        // Переходим в раздел Wiki
+        $("#wiki-tab").click();
+        //Открываем полностью список страниц и проверяем наличие SoftAssertions
+        $(byText("Show 2 more pages…")).click();
+        $("#wiki-pages-box").shouldHave(textCaseSensitive("SoftAssertions"));
 
-    //Переходим на странциу SoftAssertions и проверяем наличие примера для Junit 5
-    $(byText("SoftAssertions")).click();
-    $(".markdown-body").shouldHave(text("Using JUnit5 extend test class:"));
+        //Переходим на странциу SoftAssertions и проверяем наличие примера для Junit 5
+        $(byText("SoftAssertions")).click();
+        $(".markdown-body").shouldHave(text("Using JUnit5 extend test class:"));
 
 
     }
